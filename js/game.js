a8d1bd5d76cb98f76e9296423655dd664161637b
R0adM0VlE <Kaib27900@gmail.com>
2026-01-26 11:47:24 +0100

    opdracht

--- js/game.js
+++ js/game.js
@@ -29,7 +29,7 @@ board.addEventListener('click', (e) => {
   placeStone(x, y, current);
 
   if (checkWin(x, y, current)) {
-    alert((current === 1 ? "Zwart" : "Wit") + " wint!");
+    showMessage((current === 1 ? "Zwart" : "Wit") + " wint!");
     gameOver = true;
     return;
   }
@@ -70,28 +70,3 @@ function count(x, y, dx, dy, player) {
   }
   return n;
 }
-
-//name functie
-function saveNames() {
-    const p1 = document.getElementById("player1").value;
-    const p2 = document.getElementById("player2").value;
-
-    localStorage.setItem("player1", p1);
-    localStorage.setItem("player2", p2);
-
-    window.location.href = "game.html";
-}
-
-window.addEventListener("load", () => {
-    const p1 = localStorage.getItem("player1");
-    const p2 = localStorage.getItem("player2");
-
-    if (document.getElementById("blackName")) {
-        document.getElementById("blackName").textContent = p1;
-    }
-
-    if (document.getElementById("whiteName")) {
-        document.getElementById("whiteName").textContent = p2;
-    }
-});
-
